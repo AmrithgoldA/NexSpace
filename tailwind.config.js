3af275a13b07937e9ee57f1bcdf782f636446eb4
AmrithgoldA <goldandgold1002@gmail.com>
2024-10-07 18:20:50 +0530

    profile update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        autofillBg:  'rgb(0 ,0 ,0)',
+        autofillText: 'rgb(255, 255, 255)',
+      },
+    },
   },
   plugins: [],
 }
